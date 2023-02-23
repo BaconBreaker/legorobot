332d8cf1ef641d3b07f3f4f5d794715d82cd37a8
Thomas Christensen <51855384+BaconBreaker@users.noreply.github.com>
2023-02-23 11:39:40 +0100

    Update check_con.py

--- check_con.py
+++ check_con.py
@@ -1,8 +1,6 @@
 import rpyc
 
 try:
-    #ROBOT_HOSTNAME = "192.168.137.131" #"ev3dev.local"
-    #ROBOT_HOSTNAME = "fe80::57be:5279:1dc:8e82"
     ROBOT_HOSTNAME = "10.42.0.1"
     conn = rpyc.classic.connect(ROBOT_HOSTNAME)
     conn.execute("print('Hello Slave. I am your , master!')")
